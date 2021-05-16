1c6eea974378437b39925baf57afdede33fe8b94
Максим Колодяжный <mkolodyazhny@gmail.com>
2021-05-17 01:53:19 +0300

    итс фукен элайв

--- HW_chisl_5/HW_chisl_5.cpp
+++ HW_chisl_5/HW_chisl_5.cpp
@@ -9,14 +9,13 @@
 #undef max
 using namespace std;
 
-map<double, double> Euler(map<double, double> data, double step, double (*func)(double, double)) {
+void Euler(map<double, double>& data, double step, double (*func)(double, double)) {
     double x_i = data.rbegin()->first;
 	data[x_i + step] = data[x_i] + step * func(x_i + step / 2, data[x_i] + step * func(x_i, data[x_i]) / 2) - data[x_i];
-    return data;
     //O(h)
 }
 
-map<double, double> RungeKutt4(map<double, double> data, double acc, double step, double (*func) (double, double)) {
+void RungeKutt4(map<double, double>& data, double acc, double step, double (*func) (double, double)) {
     double x_i = data.rbegin()->first, y_i = data.rbegin()->second;
     double K1, K2, K3, K4;
     K1 = step * func(x_i, y_i);
@@ -24,14 +23,12 @@ map<double, double> RungeKutt4(map<double, double> data, double acc, double step
     K3 = step * func(x_i + step / 2, y_i + K2 / 2);
     K4 = step * func(x_i + step, y_i + K3);
     data[x_i + step] = y_i + (K1 + 2 * K2 + 2 * K3 + K4) / 6;
-    return data;
     //O(h^4)
 }
 
-map<double, double> Adams(map<double, double> data, double acc, double step, double (*func) (double, double)) {
+void Adams(map<double, double>& data, double acc, double step, double (*func) (double, double)) {
     double x_i = data.crbegin()->first, tmp;
 	data[x_i + step] = data[x_i] + step * (3 / 2 * func(x_i, data[x_i]) - 1 / 2 * func(x_i - step, data[x_i - step]));
-    return data;
     //O(h^2)
 }
 
@@ -43,7 +40,7 @@ double Derivative(double x, double y) {
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg) {
-	case WM_DESTROY:// если этого не сделать, то все ваши жалкие попытки закрыть окно будут проигнорированы
+	case WM_DESTROY:// если этого не сделать, то все ваши попытки закрыть окно будут проигнорированы
 		PostQuitMessage(0);// отправляет приложению сообщение WM_QUIT. Принимает код ошибки, который заносится в wParam сообщения WM_QUIT
 		break;
 	}
@@ -86,13 +83,14 @@ void  DrawWindow(valarray<double>* data, double(*f)(valarray<double>*, double)) 
 	double y, x;
 	y_min = data[1].min(); x_min = data[0].min();
 	y_max = data[1].max(); x_max = data[0].max();
-	diap_x = (x_max - x_min) / 10;
-	diap_y = (y_max - y_min) / 10;                             //диапазон делений для оY 
-	y_max += diap_y; y_min -= diap_y;
+	diap_x = max(fabs(x_max), fabs(x_min));
+	diap_y = max(fabs(y_max), fabs(y_min));                             //диапазон делений для оY 
+	y_max = diap_y; y_min = - diap_y;
+	x_max = diap_x;	x_min = - diap_x;
 	HDC hDC = GetDC(window);                       //настройка okna для рисования 
 	HPEN Pen = CreatePen(PS_SOLID, 1, RGB(0, 0, 0));     //ручка для разметки 
 	HPEN Pen1 = CreatePen(PS_SOLID, 2, RGB(0, 255, 0));        //ручка для графика 
-	HPEN Pen2 = CreatePen(PS_SOLID, 8, RGB(255, 0, 0));        //ручка для точек
+	HPEN Pen2 = CreatePen(PS_SOLID, 6, RGB(255, 0, 0));        //ручка для точек
 	//через прямоугольник rect описывается okno 
 	RECT rect;
 	GetClientRect(window, &rect);
@@ -107,35 +105,36 @@ void  DrawWindow(valarray<double>* data, double(*f)(valarray<double>*, double)) 
 			height = rect.bottom - rect.top;
 			SelectObject(hDC, Pen);
 			//оси координат 
-			MoveToEx(hDC, 0, height / 2, NULL);
-			LineTo(hDC, width, height / 2);
-			MoveToEx(hDC, width / 2, height, NULL);
-			LineTo(hDC, width / 2, 0);
+			MoveToEx(hDC, 0, height * (y_max) / (y_max - y_min), NULL);
+			LineTo(hDC, width, height * (y_max) / (y_max - y_min));
+			MoveToEx(hDC, width * (- x_min) / (x_max - x_min), height, NULL);
+			LineTo(hDC, width * (-x_min) / (x_max - x_min), 0);
 			//стрелка oY 
-			MoveToEx(hDC, width / 2, 0, NULL);
-			LineTo(hDC, width / 2 + 10, 10);
-			MoveToEx(hDC, width / 2 - 10, 10, NULL);
-			LineTo(hDC, width / 2, 0);
+			MoveToEx(hDC, width * (-x_min) / (x_max - x_min), 0, NULL);
+			LineTo(hDC, width * (-x_min) / (x_max - x_min) + 10, 10);
+			MoveToEx(hDC, width * (-x_min) / (x_max - x_min) - 10, 10, NULL);
+			LineTo(hDC, width * (-x_min) / (x_max - x_min), 0);
 			//стрелка оХ 
-			MoveToEx(hDC, width - 10, height / 2 - 10, NULL);
-			LineTo(hDC, width, height / 2);
-			MoveToEx(hDC, width, height / 2, NULL);
-			LineTo(hDC, width - 10, height / 2 + 10);
+			MoveToEx(hDC, width - 10, height * (y_max) / (y_max - y_min) - 10, NULL);
+			LineTo(hDC, width, height * (y_max) / (y_max - y_min));
+			MoveToEx(hDC, width, height * (y_max) / (y_max - y_min), NULL);
+			LineTo(hDC, width - 10, height * (y_max) / (y_max - y_min) + 10);
 			//деления oX
 			for (x = x_min; x <= x_max; x += diap_x) {
-				MoveToEx(hDC, width * (x - x_min) / (x_max - x_min), height / 2 - height / 250 - 1, NULL);
-				LineTo(hDC, width * (x - x_min) / (x_max - x_min), height / 2 + height / 250 + 1);
+				MoveToEx(hDC, width * (x - x_min) / (x_max - x_min), height * (y_max) / (y_max - y_min) - height / 250 - 1, NULL);
+				LineTo(hDC, width * (x - x_min) / (x_max - x_min), height * (y_max) / (y_max - y_min) + height / 250 + 1);
 			}
 			//деления oY
 			for (y = y_min; y < y_max; y += diap_y) {
-				MoveToEx(hDC, width / 2 - width / 250 - 1, height * (y_max - y) / (y_max - y_min), NULL);
-				LineTo(hDC, width / 2 + width / 250 + 1, height * (y_max - y) / (y_max - y_min));
+				MoveToEx(hDC, width * (-x_min) / (x_max - x_min) - width / 250 - 1, height * (y_max - y) / (y_max - y_min), NULL);
+				LineTo(hDC, width * (-x_min) / (x_max - x_min) + width / 250 + 1, height * (y_max - y) / (y_max - y_min));
 			}
 			//вывод графика и точек, по которому он строится
 			SelectObject(hDC, Pen1);
-			for (x = x_min; x < x_max; x += 1e-3) {
+			double step = (x_max - x_min) / 100;
+			for (x = data[0].min(); x < data[0].max(); x += step) {
 				MoveToEx(hDC, width * (x - x_min) / (x_max - x_min), height * (y_max - f(data, x)) / (y_max - y_min), NULL);
-				LineTo(hDC, width * (x + 1e-3 - x_min) / (x_max - x_min), height * (y_max - f(data, x + 1e-3)) / (y_max - y_min));
+				LineTo(hDC, width * (x + step - x_min) / (x_max - x_min), height * (y_max - f(data, x + step)) / (y_max - y_min));
 			}
 			SelectObject(hDC, Pen2);
 			for (unsigned i = 0; i < data[0].size(); ++i) {
@@ -147,18 +146,58 @@ void  DrawWindow(valarray<double>* data, double(*f)(valarray<double>*, double)) 
 	_getch();
 }
 
-double Lagrange(valarray<double>* data, double param) {
-	double tmp, LagPol = 0;
-	unsigned n = data[0].size();
+valarray<double> Gaus(vector <valarray<double>> a) {
+	unsigned n = a.size();
+	double coef;
+	valarray<double> ans(n);
 	for (unsigned i = 0; i < n; ++i) {
-		tmp = data[1][i];
-		for (unsigned j = 0; j < n; ++j) {
-			if (i == j) continue;
-			tmp *= (param - data[0][j]) / (data[0][i] - data[0][j]);
+		unsigned j;
+		for (j = i; j < n && a[j][i] == 0; ++j);
+		if (j == n) {
+			cout << "System has no solution or their quantity is infinity" << endl;
+			exit(0);
 		}
-		LagPol += tmp;
+		else if (i != j)
+			swap(a[j], a[i]);
+		for (j = 0; j < i; ++j) {
+			coef = a[j][i] / a[i][i];
+			a[j] -= a[i] * coef;
+		}
+		for (j = i + 1; j < n; ++j) {
+			coef = a[j][i] / a[i][i];
+			a[j] -= a[i] * coef;
+		}
 	}
-	return LagPol;
+	for (unsigned i = 0; i < n; ++i) {
+		ans[i] = (a[i][n] / a[i][i]);
+	}
+	return ans;
+}
+
+double Spline(valarray<double>* input, double param) {
+	if (param < input[0][0]) throw invalid_argument("Can't find Splines for such argument!");
+	vector<double> h;
+	double ans;
+	unsigned size = input[0].size(), i;
+	vector<valarray<double>> output(size);
+	valarray<double> m;
+	for (unsigned i = 0; i < size - 1; ++i) {
+		h.push_back(input[0][i + 1] - input[0][i]);
+	}
+	for (i = 0; i < size - 2; ++i) {
+		output[i].resize(size + 1, 0);
+		output[i][i + 1] = (h[i] + h[i + 1]) / 3;
+		output[i][i + 2] = h[i + 1] / 6;
+		output[i][size] = (input[1][i + 2] - input[1][i + 1]) / h[i + 1] - (input[1][i + 1] - input[1][i]) / h[i];
+	}
+	output[i].resize(size + 1, 0);
+	output[i][0] = 1;
+	output[i + 1].resize(size + 1, 0);
+	output[i + 1][size - 1] = 1;
+	m = Gaus(output);
+	for (i = 1; i < size - 1 && input[0][i] < param; ++i);
+	ans = ((m[i] * pow((param - input[0][i - 1]), 3) + m[i - 1] * pow(input[0][i] - param, 3)) / (h[i - 1] * 6) + (input[1][i] - m[i] * pow(h[i - 1], 2) / 6) * ((param - input[0][i - 1]) / h[i - 1]) + (input[1][i - 1] - m[i - 1] * pow(h[i - 1], 2) / 6) * (input[0][i] - param) / h[i - 1]);
+	return ans;
 }
 
 void MapToValarray(const map<double, double>& data, valarray<double>& data1, valarray<double>& data2) {
@@ -197,34 +236,34 @@ int main()
 	else if (choice == 1) {
 		cout << "Calculations by Euler : " << endl;
 		double last1, last2;
-		step = acc / 10.0;
 		order_acc = 1;
 		tmp_map = data;
 		do{
-			data = Euler(data, step, Derivative);
+			Euler(data, step, Derivative);
 			last1 = data.crbegin()->second;
-			tmp_map = Euler(tmp_map, step/2.0, Derivative);
-			tmp_map = Euler(tmp_map, step / 2.0, Derivative);
+			Euler(tmp_map, step/2.0, Derivative);
+			Euler(tmp_map, step / 2.0, Derivative);
 			last2 = tmp_map.crbegin()->second;
 		} while (fabs(last1 - last2) > acc * (pow(2, order_acc)-1));
 		for (const auto& el : tmp_map) {
 			cout << el.first << "\t" << el.second << endl;
 		}
 		MapToValarray(tmp_map, data1[0], data1[1]);
-		DrawWindow(data1, Lagrange);
+		DrawWindow(data1, Spline);
 	}
 	else if (choice == 2) {
 		cout << "Calculations by Runge-Kutt : " << endl;
 		double last1, last2;
 		order_acc = 4;
 		tmp_map = data;
+		double i = 0;
 		do {
-			data = RungeKutt4(data, acc, step, Derivative);
+			RungeKutt4(data, acc, step, Derivative);
 			last1 = data.crbegin()->second;
-			tmp_map = RungeKutt4(tmp_map, acc, step / 2, Derivative);
-			tmp_map = RungeKutt4(tmp_map, acc, step / 2, Derivative);
+			RungeKutt4(tmp_map, acc, step / 2, Derivative);
+			RungeKutt4(tmp_map, acc, step / 2, Derivative);
 			last2 = tmp_map.crbegin()->second;
-		} while (fabs(last1 - last2) + pow(step,4) < acc*(pow(4,order_acc)-1));
+		} while (i += pow(step, 4), fabs(last1 - last2) + i < acc);//*(pow(4,order_acc)-1));
 		for (const auto& el : tmp_map) {
 			cout << el.first << "\t" << el.second << endl;
 			unsigned size = data1[0].size();
@@ -232,27 +271,27 @@ int main()
 			data1[0][size] = el.first;
 		}
 		MapToValarray(tmp_map, data1[0], data1[1]);
-		DrawWindow(data1, Lagrange);
+		DrawWindow(data1, Spline);
 	}
 	else if (choice == 3) {
 		cout << "Calculations by Adams : " << endl;
 		tmp_map = data;
-		data = RungeKutt4(data, acc, step, Derivative);
-		tmp_map = RungeKutt4(tmp_map, acc, step, Derivative);
-		tmp_map = RungeKutt4(tmp_map, acc, step, Derivative);
+		RungeKutt4(data, acc, step, Derivative);
+		RungeKutt4(tmp_map, acc, step, Derivative);
+		RungeKutt4(tmp_map, acc, step, Derivative);
 		double last1, last2;
 		order_acc = 2;
 		do {
-			data = Adams(data, acc, step, Derivative);
+			Adams(data, acc, step, Derivative);
 			last1 = data.crbegin()->second;
-			tmp_map = Adams(tmp_map, acc, step / 2, Derivative);
-			tmp_map = Adams(tmp_map, acc, step / 2, Derivative);
+			Adams(tmp_map, acc, step / 2, Derivative);
+			Adams(tmp_map, acc, step / 2, Derivative);
 			last2 = tmp_map.crbegin()->second;
 		} while (fabs(last1 - last2) > acc * (pow(2, order_acc) - 1));
 		for (const auto& el : tmp_map) {
 			cout << el.first << "\t" << el.second << endl;
 		}
 		MapToValarray(tmp_map, data1[0], data1[1]);
-		DrawWindow(data1, Lagrange);
+		DrawWindow(data1, Spline);
 	}
 }
